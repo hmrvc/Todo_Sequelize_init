607c9e8413df099d6d7da0fd006ddbee88a92b7e
hmrvc <hmrvc@yahoo.com.tw>
2022-01-23 18:53:38 +0800

    Add middleware

--- app.js
+++ app.js
@@ -2,44 +2,35 @@ const express = require('express')
 const session = require('express-session')
 const {engine} = require('express-handlebars')
 const methodOverride = require('method-override')
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+const UsePassport = require('./config/passport')
+const routes = require('./routes')
+
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', engine({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: "TTT",
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('hello')
+UsePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+
 })
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
 
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const {name, email, password, confirmPassword} = req.body
-  User.create({name, email, password})
-    .then(user => res.redirect('/'))
-  
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`)
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const user = require('./modules/user')
+const home = require('./modules/home')
+const todo = require('./modules/todo')
+const {authenticator} = require('../middleware/auth')
+
+router.use('/users', user)
+router.use('/todos', authenticator, todo)
+router.use('/', authenticator, home)
+
+module.exports = router
