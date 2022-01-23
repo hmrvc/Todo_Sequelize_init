74803ff1ee5aa48b499af1cd6b3ad5418d211c8d
hmrvc <hmrvc@yahoo.com.tw>
2022-01-23 19:26:42 +0800

    Add todo CRUD

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,14 +2,30 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => {return res.render('index', {todos: todos})})
-    .catch((error) => {return res.status(422).json(error)})
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) {
+        console.log("User isn't exist")
+      }
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: {UserId: req.user.id}
+      })
+    })
+    .then(todos => res.render('index', {todos}))
+    .catch(error =>  console.log(error))
+
+
+  // return Todo.findAll({
+  //   raw: true,
+  //   nest: true
+  // })
+  //   .then((todos) => {return res.render('index', {todos: todos})})
+  //   .catch((error) => {return res.status(422).json(error)})
 })
 
 module.exports = router
--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -3,10 +3,60 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+//新增todo
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  
+  return Todo.create({UserId, name})
+    .then(() =>  res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//瀏覽特定todo
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', {todo: todo.toJSON()}))
+    .catch(error => console.log(error))
+})
+
+//修改todo
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+return Todo.findOne({where: {id, UserId}})
+  .then(todo => res.render('edit', {todo: todo.get()}))
+  .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const {name, isDone} = req.body
+
+  return Todo.findOne({where: {id, UserId}})
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+//刪除todo
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req. params.id
+  
+  return Todo.findOne({where: {id, UserId}})
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
